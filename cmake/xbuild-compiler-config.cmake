3eb6c68068b3dfc3f1c8ce2cb0e829824e269d4c
unknown <realxye@gmail.com>
2023-11-09 11:37:59 -0800

    Fix project configuration error

--- cmake/xbuild-compiler-config.cmake
+++ cmake/xbuild-compiler-config.cmake
@@ -2,17 +2,8 @@
 # Do not install by default
 set(CMAKE_SKIP_INSTALL_ALL_DEPENDENCY true)
 
-if(EXISTS "${ROOT}/configuration/${customer}/configuration.${project_name}.xml")
-   ...
-else()
-   ...
-endif()
-
 # Unset configuration types and re-define them
 set(RELEASE UNDEFINED)
 set(DEBUG UNDEFINED)
-set(NOOPT UNDEFINED)
-set(OPTIMIZED UNDEFINED)
-set(CMAKE_CONFIGURATION_TYPES NoOpt Optimized Release CACHE STRING "Project configurations" FORCE)
-
-set_property(GLOBAL PROPERTY DEBUG_CONFIGURATIONS NoOpt)
+set(CMAKE_CONFIGURATION_TYPES Debug Release CACHE STRING "Project configurations" FORCE)
+set_property(GLOBAL PROPERTY DEBUG_CONFIGURATIONS Debug)
