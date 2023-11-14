53de702d4319735abd3c7ec6b5278347ab0d4b85
unknown <realxye@gmail.com>
2023-11-13 16:13:08 -0800

    Fix paths

--- cmake/xbuild-init-project.cmake
+++ cmake/xbuild-init-project.cmake
@@ -1,5 +1,9 @@
 include_guard(GLOBAL)
 
+# Set path
+list(APPEND CMAKE_MODULE_PATH "${XBUILD_PRELUDE_CMAKE_DIR}")
+list(APPEND CMAKE_MODULE_PATH "${XBUILD_PRELUDE_PACKAGES_DIR}")
+
 # Include build related scripts
 include(xbuild-env)
 include(xbuild-platform)
